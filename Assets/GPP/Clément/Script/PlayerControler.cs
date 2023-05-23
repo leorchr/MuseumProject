305573bee17847d23251cd28d9d78b467a940469
vifclem <clement.expert@artfx.fr>
2023-05-23 14:48:03 +0200

    variable

--- Assets/GPP/Clément/Script/PlayerControler.cs
+++ Assets/GPP/Clément/Script/PlayerControler.cs
@@ -1,66 +1,76 @@
 
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerControler : MonoBehaviour
 {
     public static PlayerControler instance;
     [Space]
     [Header("Position Check\n--------------")]
-    [Space]
     [SerializeField] private bool isGrounded;
     
-
     [Space]
     [Header("Player info\n--------------")]
-    [Space]
     [SerializeField] private bool isFacingRight = true;
     [Range(1f, 10f)]
-    [SerializeField] private float jumpVelocity;
-    [SerializeField] private float speed = 8f;
-
-
-    private Vector3 movementForce;
-    private float fallMultiplier = 2.2f;
-    private float lowJumpMultiplier = 2f;
-    private Vector2 direction;
+    [SerializeField] private float speed;
     private Rigidbody rb;
     private InputAction controls;
-  
+
+    [Space]
+    [Header("Movement\n-----------")]
+    private Vector2 direction;
+    private Vector3 movementForce;
+
+    [Space]
+    [Header("Jump\n-----------")]
+    [Range(1f, 10f)]
+    [SerializeField] private float jumpVelocity;
+    private float fallMultiplier = 2.2f;
+    private float lowJumpMultiplier = 1.8f;
+    private bool isHolding = false;
+
+    [HideInInspector]
+    public Vector3 respawnPosition;
+    
+
+
     private void Awake()
     {
         instance = this;
         rb = GetComponent<Rigidbody>();
         controls = new InputAction();
     }
-    void Start()
-    {
-        
-    }
-
-    
+   
     void Update()
     {
-        //float horizontal = Input.GetAxis("horizontal");
-        //float vertical = Input.GetAxis("vertical");
-
-        //movementForce = new Vector3(horizontal, 0, 0);
-        
         
         transform.position += speed * Time.deltaTime * new Vector3(direction.x, 0, 0);
 
-        //betterJump
-        if(rb.velocity.y < 0)
+        //flip
+        
+
+    
+
+       
+
+        //fallJump
+        if (rb.velocity.y < 0)
         {
             rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
         }
         
+
+        
+        
     }
     private void FixedUpdate()
     {
-        
 
+       
     }
 
     private void OnEnable()
@@ -83,28 +93,50 @@ public class PlayerControler : MonoBehaviour
     {
         direction = context.ReadValue<Vector2>();
         Debug.Log(direction);
-        //rb.AddForce(movementForce * speed);
+        
        
+
     }
 
     public void Jump(InputAction.CallbackContext context)
     {
 
-        if (context.performed && isGrounded)
+        if (context.started && isGrounded)
         {
-            
+            isHolding = true;
             rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
             isGrounded = false;
 
         }
-        
+
+        if (context.canceled && !context.started)
+        {
+            isHolding = false;
+        }
+        if (context.performed && !context.started)
+        {
+            isHolding = false;
+        }
+
+
+
 
     }
 
+    public void AddJumpForce()
+    {
+        if (isHolding)
+        {
+            rb.AddForce(new Vector3(speed, 0,0), ForceMode.Impulse);
+        }
+    }
     public void Flip()
     {
        
+        
+            
     }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
