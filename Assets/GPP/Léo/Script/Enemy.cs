fe53365e0e07fde5730cb37b7e811932b0d423ce
L3o <leogravillon@hotmail.fr>
2023-05-24 12:34:29 +0200

    [wip] enemy detection

--- Assets/GPP/Léo/Script/Enemy.cs
+++ Assets/GPP/Léo/Script/Enemy.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEditor.Rendering;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    #region Enemy Movements
+
     public Transform[] target;
     private int currentTarget;
     [SerializeField] private float speed;
 
-    [SerializeField] private bool playerFocus;
+    #endregion
 
-    //temporaire
-    public float distanceFocus = 5f;
+    #region Player Detection
+
+    private RaycastHit hit;
+    [SerializeField] private Transform raycastPos;
+    [Range(1f, 50f)][SerializeField] private float viewingDistance = 5f;
+    [SerializeField] private LayerMask playerMask = ~0;
+    [SerializeField] private LayerMask obstacleMask = ~0;
+    private bool playerFocus;
+
+    #endregion
+
+    #region Debug
+    [Space]
+    [Header("Debug")]
+    [Space]
+    [SerializeField] private bool visualDebugging = true;
+    [SerializeField] private Color color = new Color(0.75f, 0.2f, 0.2f, 0.75f);
+    [Range(1f, 10f)][SerializeField] private float lineWidth = 5f;
+
+    #endregion
 
     private void Start()
     {
@@ -20,7 +41,7 @@ public class Enemy : MonoBehaviour
         playerFocus = false;
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
@@ -30,39 +51,78 @@ public class Enemy : MonoBehaviour
 
     private void Update()
     {
-        if (Vector3.Distance(transform.position, PlayerControler.instance.transform.position) > distanceFocus || Vector3.Distance(transform.position, target[0].position) > distanceFocus)
-        {
-            playerFocus = false;
-        }
-        else playerFocus = true;
-
-
-        if (playerFocus)
-        {
-            // Deplacement focus joueur
-            Debug.Log("Là je suis entrain de suivre le joueur frérot");
-            transform.position = Vector3.MoveTowards(transform.position, PlayerControler.instance.transform.position, speed * Time.deltaTime);
-            
-        }
-        else
-        {
-            if (Vector3.Distance(transform.position, target[currentTarget].transform.position) < 0.1f)
-            {
-                SelectNextTarget();
-            }
-            // Deplacement standard entre deux points
-            Debug.Log("Là je t'explique je suis entrain de me diriger vers la prochaine position qui est en : " + target[currentTarget].position);
-            transform.position = Vector3.MoveTowards(transform.position, target[currentTarget].position, speed * Time.deltaTime);
-        }
+        DetectPlayer();
+        Movements();
+        
     }
 
     private void SelectNextTarget()
     {
-        if (currentTarget < target.Length-1)
+        if (currentTarget < target.Length - 1)
         {
             currentTarget++;
         }
         else currentTarget = 0;
-        Debug.Log(currentTarget);
     }
+
+    private void FlipSprite(Vector3 target)
+    {
+        Vector2 dir = new Vector2(target.x - transform.position.x, 0);
+        float angle = dir.x > 0f ? 180f : 0f;
+        transform.rotation = new Quaternion(0, angle, 0, 0);
+    }
+
+    private void Movements()
+    {
+        // Deplacement focus joueur
+        if (playerFocus)
+        {
+            FlipSprite(PlayerControler.instance.transform.position);
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(PlayerControler.instance.transform.position.x, transform.position.y, transform.position.z), speed * Time.deltaTime);
+        }
+        // Deplacement standard entre deux points
+        else
+        {
+            //if (Vector3.Distance(transform.position, target[currentTarget].transform.position) < 0.1f) // suit la position exacte
+            if (target[currentTarget].transform.position.x - transform.position.x < 0.1f && target[currentTarget].transform.position.x - transform.position.x > -0.1f) // suit l'axe x
+            {
+                SelectNextTarget();
+            }
+            FlipSprite(target[currentTarget].transform.position);
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(target[currentTarget].position.x, transform.position.y, transform.position.z), speed * Time.deltaTime);
+        }
+    }
+
+    private void DetectPlayer()
+    {
+        //if ((PlayerControler.instance.transform.position.x - transform.position.x < 0.1f && PlayerControler.instance.transform.position.x - transform.position.x > -0.1f))
+        if (Physics.Raycast(raycastPos.position, -raycastPos.transform.right, out hit, viewingDistance, playerMask))
+        {
+            float distObject = Vector3.Distance(hit.transform.position, transform.position);
+            if (!Physics.Raycast(raycastPos.position, -raycastPos.transform.right, distObject, obstacleMask))
+            {
+                playerFocus = true;
+                Debug.Log("Je vois le joueur !");
+            }
+
+        }
+        else
+        {
+            playerFocus = false;
+            Debug.Log("Je ne vois pas le joueur");
+        }
+    }
+
+#if UNITY_EDITOR
+
+    private void OnDrawGizmosSelected()
+    {
+        if (!visualDebugging)
+            return;
+
+        Handles.color = color;
+        Handles.DrawAAPolyLine(lineWidth, raycastPos.position, raycastPos.position + -raycastPos.transform.right * viewingDistance);
+    }
+#endif
 }
+
