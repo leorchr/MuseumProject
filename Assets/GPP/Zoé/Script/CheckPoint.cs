febc589a3bb8bb208c80e1c5da66ddcd33adfa28
L3o <leogravillon@hotmail.fr>
2023-05-30 12:28:06 +0200

    Update Checkpoints materials

--- Assets/GPP/Zoé/Script/CheckPoint.cs
+++ Assets/GPP/Zoé/Script/CheckPoint.cs
@@ -6,15 +6,18 @@ public class CheckPoint : MonoBehaviour
 {
     [SerializeField] private Material materialDone;
     [SerializeField] private MeshRenderer startMesh;
+    [SerializeField] private bool onlyOnce = false;
+    private bool check = false;
 
     public void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player" && !check)
         {
             Debug.Log("New Checkpoint set to : " + transform.position);
             PlayerControler.instance.respawnPosition = transform.position;
             startMesh.material = materialDone;
             //mettre un sfx
+            if (onlyOnce) check = true;
         }
     }
 }
