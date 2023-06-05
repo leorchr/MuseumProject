f2f8f33ecb745c29e1e95f40aa1da4293366f9c4
L3o <leogravillon@hotmail.fr>
2023-06-05 10:58:28 +0200

    [refactor] switch cam

--- Assets/GPP/Léo/Script/SwitchCamera.cs
+++ Assets/GPP/Léo/Script/SwitchCamera.cs
@@ -6,14 +6,20 @@ public class SwitchCamera : MonoBehaviour
 {
     [SerializeField] private GameObject cam1, cam2;
 
+    private void Start()
+    {
+        cam1.SetActive(true);
+        cam2.SetActive(false);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        cam2.SetActive(true);
         cam1.SetActive(false);
+        cam2.SetActive(true);
     }
     private void OnTriggerExit(Collider other)
     {
-        cam2.SetActive(false);
         cam1.SetActive(true);
+        cam2.SetActive(false);
     }
 }
