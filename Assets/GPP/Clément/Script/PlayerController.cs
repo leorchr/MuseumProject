a0cae92cd99baf276c9eb7220fd270beed81a7c2
vifclem <clement.expert@artfx.fr>
2023-06-07 17:03:15 +0200

    [ VRAI FIXE ] crouch et sprint

--- Assets/GPP/Clément/Script/PlayerController.cs
+++ Assets/GPP/Clément/Script/PlayerController.cs
@@ -339,7 +339,7 @@ public class PlayerController : MonoBehaviour
 
     public void Sprint(InputAction.CallbackContext context)
     {
-        if (context.started && isGrounded)
+        if (context.started && isGrounded && !isCrouching && !isCrouchRunning)
         {
             isSprinting = true;
             moveSpeed = sprintSpeed;
@@ -362,7 +362,7 @@ public class PlayerController : MonoBehaviour
 
     public void Crouch(InputAction.CallbackContext context)
     {
-        if (context.started && isGrounded)
+        if (context.started && isGrounded && !isSprinting)
         {
             isCrouching = true;
             if (isCrouching && playerStatus == PlayerStatus.Run)
